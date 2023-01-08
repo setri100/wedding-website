0427a15a09b7a27b7ab74946b4aa1c902bb5d7f1
Sebastian Triesch <sebastiantriesch@Sebastians-MacBook-Pro-3.local>
2023-01-08 20:26:14 +0100

    update

--- js/scripts.js
+++ js/scripts.js
@@ -195,7 +195,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Hups!</strong> Der Einladungscode scheint nicht zu stimmen.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbyC_qU3FczOZGmS2e_Elr-Kci8nP8B_PmbAnzPkzO9eossZOFKw-0Bzd6233epdHhGG/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
